fcc3a4a47e81b6e88adf3487abc3d715f37558c5
samir <samirsadriuboolean@gmail.com>
2026-02-17 21:57:35 +0100

    first commit

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,37 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/pizzas")
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository repository;
+
+    @GetMapping
+    public String index(Model model){
+        List<Pizza> result = repository.findAll();
+        model.addAttribute("pizzas", result);
+        return("pizzas/index");
+
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+    Pizza pizza = repository.findById(id).orElseThrow();
+    model.addAttribute("pizza", pizza);
+    return "pizzas/pizza-by-id";
+    }
+
+}
